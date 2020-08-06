a0711f791f6e064282bc5fa46e4c5dcae13be3bb
SwarupKarmokar <swarupkarmokar123@gmail.com>
2020-08-06 13:35:39 +0530

    array updated

--- app.py
+++ app.py
@@ -47,41 +47,41 @@ def predict():
         Source = request.form['Source']
 
         if Source == 'Banglore':
-            temp_array = [1, 0, 0, 0, 0]
+            temp_array = temp_array + [1, 0, 0, 0, 0]
         elif Source == 'Chennai':
-            temp_array = [0, 1, 0, 0, 0]
+            temp_array = temp_array + [0, 1, 0, 0, 0]
         elif Source == 'Delhi':
-            temp_array = [0, 0, 1, 0, 0]
+            temp_array = temp_array + [0, 0, 1, 0, 0]
         elif Source == 'Kolkata':
-            temp_array = [0, 0, 0, 1, 0]
+            temp_array = temp_array + [0, 0, 0, 1, 0]
         elif Source == 'Mumbai':
-            temp_array = [0, 0, 0, 0, 1]
+            temp_array = temp_array + [0, 0, 0, 0, 1]
 
         Destination = request.form['Destination']
         if Destination == 'Bangalore':
-            temp_array = [1, 0, 0, 0, 0, 0]
+            temp_array = temp_array + [1, 0, 0, 0, 0, 0]
         elif Destination == 'Cochin':
-            temp_array = [0, 1, 0, 0, 0, 0]
+            temp_array = temp_array + [0, 1, 0, 0, 0, 0]
         elif Destination == 'Delhi':
-            temp_array = [0, 0, 1, 0, 0, 0]
+            temp_array = temp_array + [0, 0, 1, 0, 0, 0]
         elif Destination == 'Hyderabad':
-            temp_array = [0, 0, 0, 1, 0, 0]
+            temp_array = temp_array + [0, 0, 0, 1, 0, 0]
         elif Destination == 'Kolkata':
-            temp_array = [0, 0, 0, 0, 1, 0]
+            temp_array = temp_array + [0, 0, 0, 0, 1, 0]
         elif Destination == 'New Delhi':
-            temp_array = [0, 0, 0, 0, 0, 1]
+            temp_array = temp_array + [0, 0, 0, 0, 0, 1]
 
         Total_Stops = request.form['Total_Stops']
         if Total_Stops == '0.0':
-            temp_array = [1, 0, 0, 0, 0]
+            temp_array = temp_array + [1, 0, 0, 0, 0]
         elif Total_Stops == '1.0':
-            temp_array = [0, 1, 0, 0, 0]
+            temp_array = temp_array + [0, 1, 0, 0, 0]
         elif Total_Stops == '2.0':
-            temp_array = [0, 0, 1, 0, 0]
+            temp_array = temp_array + [0, 0, 1, 0, 0]
         elif Total_Stops == '3.0':
-            temp_array = [0, 0, 0, 1, 0]
+            temp_array = temp_array + [0, 0, 0, 1, 0]
         elif Total_Stops == '4.0':
-            temp_array = [0, 0, 0, 0, 1]
+            temp_array = temp_array + [0, 0, 0, 0, 1]
 
 
 
